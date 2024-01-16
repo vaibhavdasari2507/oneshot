922f38f1bc246030ea7849d22d5517aff6f4f015
Vaibhav Dasari <vaibhav.d20@iiits.in>
2024-01-16 22:43:42 +0530

    added delete blogs

--- backend/controllers/blogs/delete_blog.js
+++ backend/controllers/blogs/delete_blog.js
@@ -3,8 +3,29 @@ const Blog = require("../../models/blog");
 exports.delete_blog = async (req, res) => {
   try {
     const id = req.params.id;
-    const userPost = Post.findById(id);
+    let userPost = await Post.findById(id);
+    if(!userPost) {
+        return res.status(404).json({
+            status: true,
+            message: "Blog not found"
+        })
+    }
+    if(userPost.author !== req.user._id){
+        return res.status(403).json({
+            status: false,
+            message: "Don't have access to this post"
+        })
+    }
+    await Blog.findByIdAndDelete(id)
+    res.status(200).json({
+        status: true,
+        message: "Deleted Blog"
+    })
   } catch (err) {
-    
+    res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: err.message,
+    });
   }
 };
--- backend/routes/blog_route.js
+++ backend/routes/blog_route.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 const {add_blog} = require("../controllers/blogs/add_blog")
+const {delete_blog} = require("../controllers/blogs/delete_blog")
 const {is_authenticated} = require("../middleware/authentication")
 
 router.post("/blog",is_authenticated,add_blog)
-router.delete("/blog/:id",is_authenticated,)
+router.delete("/blog/:id",is_authenticated,delete_blog)
