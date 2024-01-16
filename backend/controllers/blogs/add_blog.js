5006f1883058a6d3132d248a58c304e52e223549
Vaibhav Dasari <vaibhav.d20@iiits.in>
2024-01-16 23:17:32 +0530

    blog image upload

--- backend/controllers/blogs/add_blog.js
+++ backend/controllers/blogs/add_blog.js
@@ -1,11 +1,44 @@
 const Blog = require("../../models/blog");
+const firebase = require("firebase/app");
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} = require("firebase/storage");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvH8UxfYf2T1U6bKimHTB19U2eLSCDAgU",
+  authDomain: "oneshot-8e7d7.firebaseapp.com",
+  projectId: "oneshot-8e7d7",
+  storageBucket: "oneshot-8e7d7.appspot.com",
+  messagingSenderId: "157484841137",
+  appId: "1:157484841137:web:b8fbfe0aae461ee0560a90",
+  measurementId: "G-PMX1R3K82G"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const storage = getStorage();
+
 
 exports.add_blog = async (req, res) => {
   try {
+
+    const file = req.file;
+    try {
+      const storageRef = ref(storage, file.originalname);
+      await uploadBytes(storageRef, file.buffer);
+      attach_url = await getDownloadURL(storageRef);
+    } catch (error) {
+      console.log(error);
+    }
+
     let newBlog = new Blog({
       title: req.body.title,
       content: req.body.content,
       author: req.user._id,
+      attach_url: attach_url,
     });
     newBlog = await newBlog.save();
     res.status(201).json({
