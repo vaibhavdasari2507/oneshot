6c6fe1a06420f6b9648cbe45b2275a60e960919f
Vaibhav Dasari <vaibhav.d20@iiits.in>
2024-01-16 22:47:11 +0530

    changed blogs route

--- backend/routes/blog_route.js
+++ backend/routes/blog_route.js
@@ -2,7 +2,11 @@ const { Router } = require("express");
 const router = Router();
 const {add_blog} = require("../controllers/blogs/add_blog")
 const {delete_blog} = require("../controllers/blogs/delete_blog")
+const {get_all_blogs} = require("../controllers/blogs/get_all_blogs")
+const {get_my_blogs} = require("../controllers/blogs/get_my_blogs")
 const {is_authenticated} = require("../middleware/authentication")
 
 router.post("/blog",is_authenticated,add_blog)
 router.delete("/blog/:id",is_authenticated,delete_blog)
+router.get("/feed",is_authenticated,get_all_blogs)
+router.get("/myblogs",is_authenticated,get_my_blogs)
